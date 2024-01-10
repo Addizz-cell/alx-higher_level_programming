9707cf66a381fda0eb2eb4292188fd490f912fed
fawas iwayemi <iwayemiolawale@gmail.com>
2024-01-10 05:14:39 -0800

    this should do

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+a = 10
+b = 5
+add_ = add(a, b)
+sub_ = sub(a, b)
+div_ = div(a, b)
+mul_ = mul(a, b)
+print("{} + {} = {}".format(a, b, add_))
+print("{} - {} = {}".format(a, b, sub_))
+print("{} * {} = {}".format(a, b, mul_))
+print("{} / {} = {}".format(a, b, div_))
