120e27304ea11b6ce8f6b97b12a1dfe6fa0b0d5d
fawas iwayemi <iwayemiolawale@gmail.com>
2024-01-22 14:32:54 -0800

    otilor

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    
+
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
-    
+
     return result_list
