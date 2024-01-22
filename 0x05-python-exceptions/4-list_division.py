1d27737f907036297c2bab0b6799dd4550054ceb
fawas iwayemi <iwayemiolawale@gmail.com>
2024-01-22 14:30:38 -0800

    otilor

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError, TypeError, IndexError) as e:
+            if isinstance(e, TypeError):
+                print("wrong type")
+            elif isinstance(e, ZeroDivisionError):
+                print("division by 0")
+            elif isinstance(e, IndexError):
+                print("out of range")
+            result = 0
+        finally:
+            result_list.append(result)
+    
+    return result_list
