103071b5a554728756a550820b0382046993aea1
fawas iwayemi <iwayemiolawale@gmail.com>
2024-01-30 08:49:38 -0800

    all code tested

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,12 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p);
+
+#endif /* PYTHON_H */
