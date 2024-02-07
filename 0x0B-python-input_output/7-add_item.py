ea96d3e56a88158817b62f5764ec117d2190db02
fawas iwayemi <iwayemiolawale@gmail.com>
2024-02-07 03:46:42 -0800

    advance should be ready

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,12 @@ import sys
 import os.path
 from json import dump, load
 
+
 def save_to_json_file(my_list, filename):
     """Save a list to a JSON file."""
     with open(filename, 'w', encoding='utf-8') as f:
         dump(my_list, f)
+
 
 def load_from_json_file(filename):
     """Load a list from a JSON file."""
@@ -16,15 +18,12 @@ def load_from_json_file(filename):
             return load(f)
     return []
 
+
 if __name__ == "__main__":
-    # Define the filename for the JSON file
     filename = "add_item.json"
-    
-    # Load the existing list from the JSON file
     my_list = load_from_json_file(filename)
-    
     # Extend the list with command-line arguments
     my_list.extend(sys.argv[1:])
-    
+
     # Save the updated list back to the JSON file
     save_to_json_file(my_list, filename)
