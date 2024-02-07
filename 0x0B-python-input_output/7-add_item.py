67203c78c66804a60c3a90927d5c00222f836f31
fawas iwayemi <iwayemiolawale@gmail.com>
2024-02-07 03:36:32 -0800

    advance should be ready

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,12 @@ import os.path
 from json import dump, load
 
 def save_to_json_file(my_list, filename):
+    """Save a list to a JSON file."""
     with open(filename, 'w', encoding='utf-8') as f:
         dump(my_list, f)
 
 def load_from_json_file(filename):
+    """Load a list from a JSON file."""
     if os.path.exists(filename):
         with open(filename, 'r', encoding='utf-8') as f:
             return load(f)
