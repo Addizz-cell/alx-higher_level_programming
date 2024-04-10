32974af5ff9403d05b7fe12cc1bf6ff7642e6c9a
fawas iwayemi <iwayemiolawale@gmail.com>
2024-04-10 02:23:11 -0700

    3 diff language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let c = "C is fun"
-let py = "Python is cool"
-let java = "JavaScript is amazing"
-console.log(c)
-console.log(py)
-console.log(java)
+const c = 'C is fun';
+const py = 'Python is cool';
+const java = 'JavaScript is amazing';
+console.log(c);
+console.log(py);
+console.log(java);
