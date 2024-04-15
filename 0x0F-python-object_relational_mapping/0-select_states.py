b80ed6387fed32579f7d682a5eab9869a14fbcfb
fawas iwayemi <iwayemiolawale@gmail.com>
2024-04-15 09:35:57 -0700

    1st Task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,32 @@
-#!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
 
+#!/usr/bin/python3
+"""  lists all states from the database """
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to get all states
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all rows using fetchall() method
+    states = cursor.fetchall()
+
+    # Print states
+    for state in states:
+        print(state)
+
+    # Disconnect from server
+    cursor.close()
     db.close()
